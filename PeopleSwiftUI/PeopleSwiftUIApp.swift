15c957a92c09ae6bcb2b10cdb3875f3e0cf3dfb1
ALX <alexmonac13@gmail.com>
2023-07-17 18:16:10 +0200

    Initial Commit

---
+++ PeopleSwiftUI/PeopleSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeopleSwiftUIApp.swift
+//  PeopleSwiftUI
+//
+//  Created by Alex on 17/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct PeopleSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
